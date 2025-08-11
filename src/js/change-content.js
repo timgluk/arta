1260c205af7843cb4bd46ab00d8d1017079b8c77
timgluk <timi.biz@bk.ru>
2025-08-11 22:51:54 +0300

    fix: click logick

--- src/js/change-content.js
+++ src/js/change-content.js
@@ -1,10 +1,5 @@
 document.querySelectorAll('.item__icon').forEach(btn => {
   btn.addEventListener('touchstart', () => {
-    btn.closest('.item').classList.add('touch');
-  });
-
-  btn.addEventListener('touchend', () => {
-    btn.closest('.item').classList.remove('touch');
+    btn.closest('.item').classList.toggle('touch');
   });
 });
-ы
